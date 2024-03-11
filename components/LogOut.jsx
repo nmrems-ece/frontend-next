b519370e61ad2bed1f85a40c2854a080ed483b72
Codeindeed <favouralex084@gmail.com>
2024-03-11 13:31:48 +0100

    changes

--- components/LogOut.jsx
+++ components/LogOut.jsx
@@ -5,25 +5,36 @@ import Link from "next/link";
 import { ImSwitch } from "react-icons/im";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import { Router } from "next/router";
 import { Toast, toast } from "react-hot-toast";
+
+
 
 const LogOut = () => {
   const router = useRouter();
 
+  const handleLogout = async () => {
+    try {
+      // Call your logout API endpoint
+cookie.remove('token')
+      // Clear token from state (assuming you're using a state management library like Redux or Context API)
+      // dispatch({ type: 'LOGOUT' });
+
+      // Redirect to login screen
+      router.push('/signin');
+      toast.success("logout successful")
+    } catch (error) {
+      console.error("Logout failed", error);
+      toast.error("logout failed")
+    }
+  };
+
   return (
-    <>
-      <div className="flex text-gray-600 bg-inherit hover:bg-[#49516f] hover:text-white focus h-9  justify-center mt-2 overflow-hidden">
-        <button>
-          <div className="m-1">
-            <ImSwitch />
-          </div>
-          <span className="ml-1">
-            <h1>Log Out</h1>
-          </span>
-        </button>
-      </div>
-    </>
+    <div>
+      <di><Toast/></di>
+    <button className="flex text-gray-600 bg-inherit hover:bg-[#49516f] hover:text-white focus h-9  justify-center mt-2 overflow-hidden" onClick={handleLogout}>
+      Log Out
+    </button>
+    </div>
   );
 };
 
